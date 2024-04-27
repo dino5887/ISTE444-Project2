af0e090dd0c40f75493939cf76348985f22dbd30
Michael M <mjm6048@rit.edu>
2024-04-27 16:06:50 -0400

    Added downloadImages route and needed imports

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect
-from pymongo import MongoClient 
+from pymongo import MongoClient
+import requests
+# python -m pip install requests
+import json
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
@@ -27,3 +31,12 @@ def index():
 @app.route('/about')
 def about():
     return '<h3>This is a Flask web application.</h3>'
+
+@app.route('/downloadImages', methods=['GET'])
+def download():
+    req = requests.get('https://pokeapi.co/api/v2/pokemon-form/132/')
+    data = json.loads(req.content)
+    return render_template('downloadImagesTest.html', data=data)
+# data=data['all']
+# data=data['property tag we specifically we want from the data so front_default']
+# '<h3>This is a Flask web application.</h3>'
